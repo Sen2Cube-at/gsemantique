a0e2cd50e722712c098aeeae1fd80da19df7d611
Felix Kröber <felixkroeber@web.de>
2025-02-10 18:33:34 +0100

    preview 0.39.0 (deps: pinned numpy and stackstac versions)

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ dependencies = [
     "ipykernel",
     "matplotlib",
     "multiprocess",
-    "pandas>=2.0.0",
+    "numpy<2.0.0",
     "planetary-computer @ git+https://github.com/fkroeber/planetary-computer-sdk-for-python.git",
     "rioxarray<=0.15.5",
     "semantique @ git+https://github.com/fkroeber/semantique.git@latest",
     "stac-asset==0.4.0",
-    "stackstac @ git+https://github.com/fkroeber/stackstac.git",
+    "stackstac @ git+https://github.com/fkroeber/stackstac.git@fix_v1",
     "tqdm",
 ]
 
